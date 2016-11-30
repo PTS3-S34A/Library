ba996a8402e4e69312364c225135866adb571d9c
Robin Laugs <Laugslander@users.noreply.github.com>
2016-11-30 18:38:48 +0100

    Introduced the new MapConstants class that replaced the constant value in the DisplayConstants class in the User-Interface project.

---
+++ Soccar [Library]/src/nl/soccar/library/MapConstants.java
@@ -0,0 +1,28 @@
+package nl.soccar.library;
+
+/**
+ *
+ * @author PTS34A
+ */
+public final class MapConstants {
+
+    /**
+     * Map constants in JBox2D units.
+     */
+    public static final float MAP_WIDTH = 160.0F;
+    public static final float MAP_HEIGHT = 90.0F;
+    public static final float SCOREBOARD_WIDTH = 10.0F;
+    public static final float SCOREBOARD_HEIGHT = 6.0F;
+    public static final float LINE_WIDTH = 0.2F;
+    public static final float CENTRE_CIRCLE_SIZE = 13.75F;
+    public static final float CENTRE_SPOT_SIZE = 1.5F;
+    public static final float FIELD_MARGIN = 5.0F;
+    public static final float BOX_WIDTH = 12.5F;
+    public static final float BOX_HEIGHT = 32.5F;
+    public static final float GOAL_WIDTH = 3.5F;
+    public static final float GOAL_HEIGHT = BOX_HEIGHT / 1.5F;
+
+    private MapConstants() {
+    }
+
+}
