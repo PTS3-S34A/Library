ee8ee81df34c6e178dbc8c0e2732dba7889d0a6c
Robin Laugs <Laugslander@users.noreply.github.com>
2016-12-05 11:26:05 +0100

    Updated the nl.soccar.exception unittests.

---
+++ Soccar [Library]/test/nl/soccar/exception/test/RoomExceptionTest.java
@@ -0,0 +1,34 @@
+package nl.soccar.exception.test;
+
+import nl.soccar.exception.RoomException;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test that tests the nl.soccar.exception.RoomExceptionTest class.
+ *
+ * @author PTS34A
+ */
+public class RoomExceptionTest {
+
+    // Declaration of test object.
+    private RoomException exception;
+
+    /**
+     * Instantiation of test object.
+     */
+    @Before
+    public void setUp() {
+        exception = new RoomException("title", "message");
+    }
+
+    /**
+     * Tests the getTitle method.
+     */
+    @Test
+    public void getTitleTest() {
+        assertEquals("title", exception.getTitle());
+    }
+
+}
