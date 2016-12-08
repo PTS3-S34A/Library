ec03007f219947ffa9b5bb8d91ac27249fa84217
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-12-08 14:33:45 +0100

    Fixed enums.

--- Soccar [Library]/src/nl/soccar/library/enumeration/BallType.java
+++ Soccar [Library]/src/nl/soccar/library/enumeration/BallType.java
@@ -7,8 +7,31 @@ package nl.soccar.library.enumeration;
  */
 public enum BallType {
 
-    FOOTBALL,
-    HOCKEY,
-    BOWLING
+    FOOTBALL(0),
+    HOCKEY(1),
+    BOWLING(2);
+    
+    private final int id;
+
+    private BallType(int id) {
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public static BallType parse(int id) {
+        switch (id) {
+            case 0:
+                return FOOTBALL;
+            case 1:
+                return HOCKEY;
+            case 2:
+                return BOWLING;
+            default:
+                throw new IllegalArgumentException("Invalid id.");
+        }
+    }
 
 }
--- Soccar [Library]/src/nl/soccar/library/enumeration/Duration.java
+++ Soccar [Library]/src/nl/soccar/library/enumeration/Duration.java
@@ -7,8 +7,31 @@ package nl.soccar.library.enumeration;
  */
 public enum Duration {
     
-    MINUTES_3,
-    MINUTES_5,
-    MINUTES_10
+    MINUTES_3(0),
+    MINUTES_5(1),
+    MINUTES_10(2);
+    
+    private final int id;
+
+    private Duration(int id) {
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public static Duration parse(int id) {
+        switch (id) {
+            case 0:
+                return MINUTES_3;
+            case 1:
+                return MINUTES_5;
+            case 2:
+                return MINUTES_10;
+            default:
+                throw new IllegalArgumentException("Invalid id.");
+        }
+    }
     
 }
